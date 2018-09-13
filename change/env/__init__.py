816819d47351b5813a8909dd629a658deb37ce1a
dtaliy <982995037@qq.com>
2018-09-13 22:40:27 +0800

    commit

--- change/env/__init__.py
+++ change/env/__init__.py
@@ -2,12 +2,19 @@ import os
 import subprocess
 def env():
     docker = subprocess.getstatusoutput("sudo docker ps | awk '{print $1}'")
-    if docker[1] != 'CONTAINER':
+    if docker[1] != "CONTAINER":
         os.system("sudo curl -sSL https://get.daocloud.io/docker | sh &&")
-    command = """
+    docker_compose = subprocess.getstatusoutput("ls /usr/local/bin | grep 'docker-compose'")
+    command_env = """
         sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&
         sudo chmod +x /usr/local/bin/docker-compose
+        """
+    if docker_compose != "docker-compose":
+        os.system(command_env)
+
+
+    command_file = """
         sudo git clone https://github.com/dtaliy/docker-typecho.git
         sudo git clone https://github.com/typecho/typecho.git
     """
-    os.system(command)
+    os.system(command_file)
