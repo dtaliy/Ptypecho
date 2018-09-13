bdbf40c2473e5594b1461943e866436a8235da99
dtaliy <982995037@qq.com>
2018-09-13 18:36:41 +0800

    commit

--- change/env/__init__.py
+++ change/env/__init__.py
@@ -4,7 +4,7 @@ def env():
         sudo curl -sSL https://get.daocloud.io/docker | sh &&
         sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&
         sudo chmod +x /usr/local/bin/docker-compose
-        sudo git clone https://github.com/scofieldpeng/docker-typecho.git
+        sudo git clone https://github.com/dtaliy/docker-typecho.git
         sudo git clone https://github.com/typecho/typecho.git
     """
     os.system(command)
