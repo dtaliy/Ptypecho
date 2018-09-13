5eb9876d1e66a163c0c96fcf73a1f99ef76d837b
dtaliy <982995037@qq.com>
2018-09-13 18:44:50 +0800

    commit

--- change/choose/__init__.py
+++ change/choose/__init__.py
@@ -5,9 +5,10 @@ def choose():
         key = input("please chose installation method:(input 1 or 2)\n"
                     "1) Semi-automatic installation(no web config)\n"
                     "2) Full automatic installation(auto web config)\n")
-    if key == 1:
+    if key == 2:
         return 1
     else:
         return 0
 
-
+a = choose()
+print(a)
