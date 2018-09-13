885153747c2fe57d6215f8e7e8f9011ca4db4618
dtaliy <982995037@qq.com>
2018-09-13 18:13:53 +0800

    commit

--- change/choose/__init__.py
+++ change/choose/__init__.py
@@ -1,6 +1,7 @@
+keylist = ['1','2']
 def choose():
-    key = 0
-    while key != 1 or key != 2:
+    key = '0'
+    while key not in keylist:
         key = input("please chose installation method:(input 1 or 2)\n"
                     "1) Semi-automatic installation(no web config)\n"
                     "2) Full automatic installation(auto web config)\n")
--- change/env/__init__.py
+++ change/env/__init__.py
@@ -4,7 +4,7 @@ def env():
         sudo curl -sSL https://get.daocloud.io/docker | sh &&
         sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&
         sudo chmod +x /usr/local/bin/docker-compose
-        sudo git cone https://github.com/scofieldpeng/docker-typecho.git
-        sudo git cone https://github.com/typecho/typecho.git
+        sudo git clone https://github.com/scofieldpeng/docker-typecho.git
+        sudo git clone https://github.com/typecho/typecho.git
     """
     os.system(command)
